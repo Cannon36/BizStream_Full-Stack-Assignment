de7f5a4bf3ee661af7f730b5ede18d8a61d137e5
Cannon_36 <cameroneidenier@gmail.com>
2021-03-06 16:30:12 -0500

    Initial Commit

---
+++ BizStream_Full-Stack-Assignment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BizStream_Full_Stack_Assignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
