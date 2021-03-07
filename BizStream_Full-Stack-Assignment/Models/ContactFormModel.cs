0e6a31f3f10966b5018c4bf8c2af70232f8507d7
Cannon_36 <cameroneidenier@gmail.com>
2021-03-06 22:08:47 -0500

    Added data annotation to validate that the ContactFormModel to make sure that the data is being enterd into the required fields and that the email addres is in a valid format.

--- BizStream_Full-Stack-Assignment/Models/ContactFormModel.cs
+++ BizStream_Full-Stack-Assignment/Models/ContactFormModel.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace BizStream_Full_Stack_Assignment.Models
 {
     public class ContactFormModel
     {
+        [Required (ErrorMessage = "Please Enter your first name.")]
         public string FirstName { get; set; }
+        [Required (ErrorMessage = "Please Enter your last name.")]
         public string LastName { get; set; }
+        [Required(ErrorMessage = "Please Enter your Email address.")]
+        [EmailAddress (ErrorMessage = "Please Enter a valid Email address.")]
         public string Email { get; set; }
+        [Required (ErrorMessage = "Please write a message before sending.")]
         public string Message { get; set; }
     }
 }
